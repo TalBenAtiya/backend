95ceecf113f998fe69b301b170365d5f884ad76d
TalBenAtiya <benatia.tal@gmail.com>
2022-11-21 16:16:55 +0200

    deploy render

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const http = require('http').createServer(app)
 app.use(cookieParser())
 app.use(express.json())
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')))
+    app.use(express.static('public'))
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
@@ -25,7 +25,7 @@ const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
 const gameRoutes = require('./api/game/game.routes')
 
-const {setupSocketAPI} = require('./services/socket.service')
+const { setupSocketAPI } = require('./services/socket.service')
 const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
 app.all('*', setupAsyncLocalStorage)
 
@@ -34,13 +34,12 @@ app.use('/api/user', userRoutes)
 app.use('/api/game', gameRoutes)
 setupSocketAPI(http)
 
-
-app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
-
 const logger = require('./services/logger.service')
-const port = process.env.PORT
-http.listen(port, () => {
-    logger.info('Server is running on port: ' + port)
+const port = process.env.PORT || 3030;
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+app.listen(port, () => {
+    logger.info(`App listening on port ${port}!`)
+});
